1f6b356c41aa126e15322564a18935d1b5f74b00
Vlad_Deineko <vladd1904@gmail.com>
2025-05-12 17:52:04 +0300

    update

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,5 +1,5 @@
-function getMessage() {
-  return new Promise<string>((resolve) => {
+function getMessage(): Promise<string> {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve("Hello from TS");
     }, 1000);
